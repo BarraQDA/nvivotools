334ceea7878e3052d10b0c24bb4023166ee5a95f
Jonathan Schultz <jonathan@imatix.com>
2016-07-08 14:16:33 +1000

    Attach drops before creating user.

--- Windows/attach.bat
+++ Windows/attach.bat
@@ -17,5 +17,6 @@ copy "%~1" %TEMPDIR%
 icacls "%TEMPDIR%\%~n1%~x1" /grant Everyone:(OI)(CI)F
 sqlcmd -S %server% -Q "CREATE DATABASE %DB% ON (FILENAME='%TEMPDIR%\%~n1%~x1') FOR ATTACH"
 rem sqlcmd -S %server% -Q "CREATE LOGIN nvivotools WITH PASSWORD='nvivotools'"
+sqlcmd -S %server% -Q "use %DB%; DROP USER nvivotools"
 sqlcmd -S %server% -Q "use %DB%; CREATE USER nvivotools FROM LOGIN nvivotools"
 sqlcmd -S %server% -Q "use %DB%; GRANT CONTROL TO nvivotools"
