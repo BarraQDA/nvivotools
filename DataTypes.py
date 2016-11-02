f7cf0bdfb6f5a37100b39acb5eee2433e2325214
Jonathan Schultz <jonathan@imatix.com>
2016-11-02 17:31:40 +1100

    Added author to copyright messages

--- DataTypes.py
+++ DataTypes.py
@@ -1,3 +1,6 @@
+#
+# Copyright 2016 Jonathan Schultz
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
--- DenormaliseNVPX.py
+++ DenormaliseNVPX.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
+# Copyright 2016 Jonathan Schultz
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
