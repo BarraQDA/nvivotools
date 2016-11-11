89919a1aa13d8719113bc6418812a352ee5d96e0
Jonathan Schultz <jonathan@imatix.com>
2016-11-11 17:43:00 +1100

    Don't redirect output of attach

--- Windows/mssqlAttach.bat
+++ Windows/mssqlAttach.bat
@@ -36,7 +36,7 @@ set server=%COMPUTERNAME%\%INSTANCE%
 del "%TEMPDIR%\%~n1*" >nul 2>&1
 copy "%~1" %TEMPDIR% >nul
 icacls "%TEMPDIR%\%~n1%~x1" /grant Everyone:(OI)(CI)F >nul
-sqlcmd -S %server% -Q "CREATE DATABASE %DB% ON (FILENAME='%TEMPDIR%\%~n1%~x1') FOR ATTACH" >nul
+sqlcmd -S %server% -Q "CREATE DATABASE %DB% ON (FILENAME='%TEMPDIR%\%~n1%~x1') FOR ATTACH"
 sqlcmd -S %server% -Q "CREATE LOGIN nvivotools WITH PASSWORD='nvivotools'"
 sqlcmd -S %server% -Q "use %DB%; IF EXISTS (SELECT * FROM sys.database_principals WHERE name = N'nvivotools') DROP USER nvivotools" >nul
 sqlcmd -S %server% -Q "use %DB%; CREATE USER nvivotools FROM LOGIN nvivotools" >nul
