ce843deee91b50fd5c9a4ea16d75e8389541cab3
Jonathan Schultz <jonathan@imatix.com>
2016-12-12 13:13:05 +1100

    Added support for application-only authentication. Requires https://github.com/BarraQDA/python-twitter/commit/f2dd2d35a18428407017c6bbc1fe986c94e86fd0

--- twitterSearch.py
+++ twitterSearch.py
@@ -11,9 +11,20 @@ import string
 import pytz
 from datetime import datetime
 
+
+import warnings
+warnings.simplefilter("error", DeprecationWarning)
+
 parser = argparse.ArgumentParser(description='Mass twitter search.')
 
 parser.add_argument('-v', '--verbosity', type=int, default=1)
+
+# Twitter authentication stuff
+parser.add_argument('-a', '--application-only-auth', action='store_true')
+parser.add_argument('--access-token-key', type=str,
+                    help='Access token key for Twitter authentication')
+parser.add_argument('--access-token-secret', type=str,
+                    help='Access token secret for Twitter authentication')
 
 parser.add_argument('-c', '--count',  type=int, help='Number of tweets to return', default=100)
 
@@ -27,54 +38,52 @@ parser.add_argument('search', type=str, help='Search string.')
 
 args = parser.parse_args()
 
-## Hard code keys for now
+# Twitter URLs
+REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
+SIGNIN_URL = 'https://api.twitter.com/oauth/authenticate'
+
+# Hard code keys for now
 consumer_key='ODCA5b8DKy7vi0CShoaWhCqvs'
 consumer_secret='YTQZgVxSzMGKbbyTug5kU18GmTGrq89DsGNQbGIfFTEFyF2ZR9'
 
-#REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
-#ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
-#AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
-#SIGNIN_URL = 'https://api.twitter.com/oauth/authenticate'
+if args.application_only_auth:
+    api = twitter.Api(
+                consumer_key=consumer_key,
+                consumer_secret=consumer_secret,
+                application_only_auth=True
+        )
+else:
+    if args.access_token_key is None or args.access_token_secret is None:
+        oauth_client = OAuth1Session(consumer_key, client_secret=consumer_secret, callback_uri='oob')
 
-#oauth_client = OAuth1Session(consumer_key, client_secret=consumer_secret, callback_uri='oob')
+        resp = oauth_client.fetch_request_token(REQUEST_TOKEN_URL)
+        url = oauth_client.authorization_url(AUTHORIZATION_URL)
 
-#print('\nRequesting temp token from Twitter...\n')
+        sys.stderr.write('Opening browser for Twitter authentication: ' + url + '\n')
 
-#try:
-    #resp = oauth_client.fetch_request_token(REQUEST_TOKEN_URL)
-#except ValueError as e:
-    #raise 'Invalid response from Twitter requesting temp token: {0}'.format(e)
+        webbrowser.open(url)
+        sys.stderr.write('Enter your pincode? ')
+        pincode = raw_input()
 
-#url = oauth_client.authorization_url(AUTHORIZATION_URL)
+        oauth_client = OAuth1Session(consumer_key, client_secret=consumer_secret,
+                                    resource_owner_key=resp.get('oauth_token'),
+                                    resource_owner_secret=resp.get('oauth_token_secret'),
+                                    verifier=pincode)
+        resp = oauth_client.fetch_access_token(ACCESS_TOKEN_URL)
+        args.access_token_key = resp.get('oauth_token')
+        args.access_token_secret = resp.get('oauth_token_secret')
 
-#print('I will try to start a browser to visit the following Twitter page '
-        #'if a browser will not start, copy the URL to your browser '
-        #'and retrieve the pincode to be used '
-        #'in the next step to obtaining an Authentication Token: \n'
-        #'\n\t{0}'.format(url))
+        sys.stderr.write('To re-use access token next time use the following arguments:\n')
+        sys.stderr.write('    --access-token-key ' + args.access_token_key + ' --access-token-secret ' + args.access_token_secret + '\n')
 
-#webbrowser.open(url)
-#pincode = raw_input('\nEnter your pincode? ')
-
-#print('\nGenerating and signing request for an access token...\n')
-
-#oauth_client = OAuth1Session(consumer_key, client_secret=consumer_secret,
-                                #resource_owner_key=resp.get('oauth_token'),
-                                #resource_owner_secret=resp.get('oauth_token_secret'),
-                                #verifier=pincode)
-#try:
-    #resp = oauth_client.fetch_access_token(ACCESS_TOKEN_URL)
-#except ValueError as e:
-    #raise 'Invalid response from Twitter requesting temp token: {0}'.format(e)
-
-#access_token_key = resp.get('oauth_token')
-#access_token_secret = resp.get('oauth_token_secret')
-
-api = twitter.Api(
-            consumer_key=consumer_key,
-            consumer_secret=consumer_secret,
-            application_only_auth=True
-      )
+    api = twitter.Api(
+                consumer_key=consumer_key,
+                consumer_secret=consumer_secret,
+                access_token_key=args.access_token_key,
+                access_token_secret=args.access_token_secret
+        )
 
 searchlc = args.search.lower()
 totaltweets = 0
