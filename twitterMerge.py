fc77632f9c866e76dad1eba29b0ffbe87c173496
Jonathan Schultz <jonathan@imatix.com>
2017-01-01 10:22:58 +0800

    More fixes

--- twitterMerge.py
+++ twitterMerge.py
@@ -110,14 +110,13 @@ if args.until is not None and args.until > (0 if headidx is None else currow[hea
     currowitem = nextornone(twitterreader)
     currow[twitteridx] = currowitem
     if currowitem is not None:
-        print (0)
         openfiles += 1
         inreader[twitteridx] = twitterreader
         currowitem['date'] = currowitem['datetime'].isoformat()
         if headidx is None or currowitem['id'] > currow[headidx]['id']:
             headidx = twitteridx
     else:
-        print (1)
+        twitterreader = None
         if args.verbosity > 1:
             print("Closing twitter feed", file=sys.stderr)
 
@@ -134,8 +133,10 @@ outunicodecsv.writeheader()
 
 while True:
     outunicodecsv.writerow(currow[headidx])
+    rowcnt[headidx] += 1
     lastid = currow[headidx]['id']
     lastdate = currow[headidx]['date']
+
 
     #for fileidx in range(len(inreader)):
         #if currow[fileidx] is not None and matching[fileidx]:
@@ -147,17 +148,9 @@ while True:
         break
 
     for fileidx in range(len(inreader)):
-        if currow[fileidx] is not None and matching[fileidx]:
+        if currow[fileidx] is not None and currow[fileidx]['id'] == currow[headidx]['id']:
             currow[fileidx] = nextornone(inreader[fileidx])
-            if currow[fileidx] is not None:
-                # Test for blank record in CSV
-                if currow[fileidx]['id'] == '':
-                    currow[fileidx] = nextornone(inreader[fileidx])
-                    matching[fileidx] = False
-                else:
-                    if 'date' not in currow[fileidx].keys():
-                        currow[fileidx]['date'] = currow[fileidx]['datetime'].isoformat()
-            else:
+            if currow[fileidx] is None:
                 if args.verbosity > 1:
                     print("Closing " + args.infile[fileidx] + " after " + str(rowcnt[fileidx]) + " rows.", file=sys.stderr)
                 rowcnt[fileidx] = 0
@@ -167,22 +160,37 @@ while True:
                 # Forget exhausted twitter feed since it cannot be re-used
                 if fileidx == twitteridx:
                     twitterreader = None
-
+            # Test for blank record in CSV
+            elif currow[fileidx]['id'] == '':
+                currow[fileidx] = None
 
     headidx = None
     for fileidx in range(len(inreader)):
         if currow[fileidx] is not None and (headidx is None or currow[fileidx]['id'] > currow[headidx]['id']):
             headidx = fileidx
 
+    nextheadidx = headidx
     for fileidx in range(len(inreader)):
-        if currow[fileidx] is not None:
-            if matching[fileidx]:
-                if currow[fileidx]['id'] != currow[headidx]['id']:
-                    print("WARNING: Missing tweet, id: " + currow[fileidx]['id'] + " in file: " + args.infile[fileidx], file=sys.stderr)
-                    matching[fileidx] = False
-            else:
-                if currow[fileidx]['id'] == currow[headidx]['id']:
-                    matching[fileidx] = True
+        if inreader[fileidx] is not None:
+            if currow[fileidx] is None:
+                currow[fileidx] = nextornone(inreader[fileidx])
+                matching[fileidx] = False
+                if currow[fileidx] is not None and (nextheadidx is None or currow[fileidx]['id'] > currow[nextheadidx]['id']):
+                    nextheadidx = fileidx
+
+            if currow[fileidx] is not None:
+                if 'date' not in currow[fileidx].keys():
+                    currow[fileidx]['date'] = currow[fileidx]['datetime'].isoformat()
+
+                if matching[fileidx]:
+                    if currow[fileidx]['id'] != currow[headidx]['id']:
+                        print("WARNING: Missing tweet, id: " + currow[fileidx]['id'] + " in file: " + args.infile[fileidx], file=sys.stderr)
+                        matching[fileidx] = False
+                elif headidx is not None:
+                    if currow[fileidx]['id'] == currow[headidx]['id']:
+                        matching[fileidx] = True
+
+    headidx = nextheadidx
 
     # Cancel twitter reader if it is now matched by an input file
     if inreader[twitteridx] is not None and len([fileidx for fileidx in range(len(inreader) - 1) if matching[fileidx]]) > 0:
@@ -196,10 +204,6 @@ while True:
         matching[twitteridx] = False
         inreader[twitteridx] = None
         openfiles -= 1
-
-    for fileidx in range(len(inreader)):
-        if matching[fileidx]:
-            rowcnt[fileidx] += 1
 
     # If no file is now matching, try opening a twitter feed
     if len([idx for idx in range(len(inreader)) if matching[idx]]) == 0:
@@ -241,6 +245,7 @@ while True:
             if headidx is None or currowitem['id'] > currow[headidx]['id']:
                 headidx = twitteridx
         else:
+            twitterreader = None
             if args.verbosity > 1:
                 print("Closing twitter feed", file=sys.stderr)
 
