dabbb97b40d84d27f466ded8c28beaab570020cc
Jonathan Schultz <jonathan@schultz.la>
2017-06-02 13:10:30 +1000

    Added code to build coded text field

--- editSources.py
+++ editSources.py
@@ -40,27 +40,73 @@ def editSources(arglist):
 
     parser.add_argument('-v', '--verbosity',  type=int, default=1)
 
-    parser.add_argument('-i', '--infile', type = str, help = 'CSV file containing source information')
+    parser.add_argument('-i', '--infile',  type = str, help = 'Input CSV file')
+    parser.add_argument('-C', '--columns', type = str, nargs = '*',
+                                           help = 'Columns from input CSV file to include as attributes')
+    parser.add_argument(      '--exclude', type = str, nargs = '*', default = [],
+                                           help = 'Columns from input CSV file to exclude as attributes')
+    parser.add_argument('--textcolumns',   type = str, nargs = '*',
+                                           help = 'Columns from input CSV file to include as coded text')
 
     parser.add_argument('-n', '--name',        type = lambda s: unicode(s, 'utf8'))
     parser.add_argument('-d', '--description', type = lambda s: unicode(s, 'utf8'))
     parser.add_argument('-c', '--category',    type = lambda s: unicode(s, 'utf8'))
     parser.add_argument('-a', '--attributes',  type = str, action='append', help='Attributes in format name:value')
     parser.add_argument(      '--color',       type = str)
-    parser.add_argument('-s', '--source',      type = str,
-                        help = 'Source file name')
-    parser.add_argument('-t', '--text',        type = str,
-                        help = 'Source text')
+    parser.add_argument('-s', '--source',      type = str, help = 'Source file name')
+    parser.add_argument('-t', '--text',        type = str, help = 'Source text')
 
     parser.add_argument('-u', '--user',        type = lambda s: unicode(s, 'utf8'),
-                        help = 'User name, default is project "modified by".')
+                                               help = 'User name, default is project "modified by".')
 
-    parser.add_argument('normFile', type=str)
+    parser.add_argument('--no-comments', action='store_true', help='Do not produce a comments logfile')
+
+    parser.add_argument('-o', '--outfile',  type=str, help='Output normalised NVivo (.norm) file')
+    parser.add_argument(        'infile',   type=str, help='Input CSV file')
 
     args = parser.parse_args()
+    hiddenargs = ['verbosity']
 
     try:
-        norm = NVivoNorm(args.normFile)
+        incomments = ''
+        if args.infile:
+            csvFile = file(args.infile, 'r')
+
+            # Skip comments at start of CSV file.
+            while True:
+                line = csvFile.readline()
+                if line[:1] == '#':
+                    incomments += line
+                else:
+                    csvfieldnames = next(unicodecsv.reader([line]))
+                    break
+
+        if not args.no_comments:
+            logfilename = args.outfile.rsplit('.',1)[0] + '.log'
+
+            comments = (' ' + args.outfile + ' ').center(80, '#') + '\n'
+            comments += '# ' + os.path.basename(sys.argv[0]) + '\n'
+            arglist = args.__dict__.keys()
+            for arg in arglist:
+                if arg not in hiddenargs:
+                    val = getattr(args, arg)
+                    if type(val) == str or type(val) == unicode:
+                        comments += '#     --' + arg + '="' + val + '"\n'
+                    elif type(val) == bool and val:
+                        comments += '#     --' + arg + '\n'
+                    elif type(val) == list:
+                        for valitem in val:
+                            if type(valitem) == str:
+                                comments += '#     --' + arg + '="' + valitem + '"\n'
+                            else:
+                                comments += '#     --' + arg + '=' + str(valitem) + '\n'
+                    elif val is not None:
+                        comments += '#     --' + arg + '=' + str(val) + '\n'
+
+            with open(logfilename, 'w') as logfile:
+                logfile.write(comments + incomments)
+
+        norm = NVivoNorm(args.outfile)
         norm.begin()
 
         datetimeNow = datetime.utcnow()
@@ -103,18 +149,6 @@ def editSources(arglist):
                 })
 
         if args.infile:
-            csvFile = file(args.infile, 'r')
-
-            # Skip comments at start of CSV file.
-            incomments = ''
-            while True:
-                line = csvFile.readline()
-                if line[:1] == '#':
-                    incomments += line
-                else:
-                    csvfieldnames = next(unicodecsv.reader([line]))
-                    break
-
             csvreader=unicodecsv.DictReader(csvFile, fieldnames=csvfieldnames)
             sourceRows = []
             for row in csvreader:
@@ -153,92 +187,118 @@ def editSources(arglist):
                     sourceRow[attName] = sourceRow.get(attName, attValue)
 
         sourceAttributes = {}
-        for attributeName in colNames:
-            # Skip reserved attribute names
-            if attributeName in ['Name', 'Description', 'Category', 'Color', 'Source', 'Text']:
-                continue
+        sourceNodeId = {}
+        for colName in colNames:
+            # Does column define an attribute?
+            if (not args.columns or colName in args.columns) and colName not in ['Name', 'Description', 'Category', 'Color', 'Source', 'Text'] + args.exclude + args.textcolumns:
 
-            # Determine whether attribute is already defined
-            sourceattribute = norm.con.execute(select([
-                    norm.SourceAttribute.c.Id,
-                    norm.SourceAttribute.c.Type,
-                    norm.SourceAttribute.c.Length
-                ]).where(
-                    norm.SourceAttribute.c.Name == bindparam('Name')
-                ), {
-                    'Name': attributeName
-                }).first()
+                # Determine whether attribute is already defined
+                sourceattribute = norm.con.execute(select([
+                        norm.SourceAttribute.c.Id,
+                        norm.SourceAttribute.c.Type,
+                        norm.SourceAttribute.c.Length
+                    ]).where(
+                        norm.SourceAttribute.c.Name == bindparam('Name')
+                    ), {
+                        'Name': colName
+                    }).first()
 
-            if sourceattribute:
-                sourceAttributes[attributeName] = {
-                    'Id':     sourceattribute['Id'],
-                    'Type':   sourceattribute['Type'],
-                    'Length': sourceattribute['Length']
-                }
-            else:
-                attributeId = uuid.uuid4()
-                typeInteger = True
-                typeDecimal = True
-                typeDateTime = True
-                typeDate = True
-                typeTime = True
-                typeBoolean = True
-                attributeLength = 0
-                for sourceRow in sourceRows:
-                    attributeValue = sourceRow[attributeName]
-                    attributeLength = max(attributeLength, len(attributeValue))
-                    try:
-                        int(attributeValue)
-                    except ValueError:
-                        typeInteger = False
-                    try:
-                        float(attributeValue)
-                    except ValueError:
-                        typeDecimal = False
-                    try:
-                        datetimeval = dateparser.parse(attributeValue, default=datetime.min)
-                        if datetimeval.hour or datetimeval.minute:
-                            typeDate = False
-                        # Assume date being min means taken from default, ie not specified in datetime
-                        if datetimeval.date() != datetime.min.date():
-                            typeTime = False
-                    except ValueError:
-                        typeDateTime = False
-                        typeDate = False
-                        typeTime = False
-                    if not attributeValue.lower() in {'true', 'false'}:
-                        typeBoolean = False
-                if typeInteger:
-                    attributeType = 'integer'
-                elif typeDecimal:
-                    attributeType = 'decimal'
-                elif typeBoolean:
-                    attributeType = 'boolean'
-                elif typeDate:
-                    attributeType = 'date'
-                elif typeTime:
-                    attributeType = 'time'
-                elif typeDateTime:
-                    attributeType = 'datetime'
+                if sourceattribute:
+                    sourceAttributes[colName] = {
+                        'Id':     sourceattribute['Id'],
+                        'Type':   sourceattribute['Type'],
+                        'Length': sourceattribute['Length']
+                    }
                 else:
-                    attributeType = 'text'
+                    attributeId = uuid.uuid4()
+                    typeInteger = True
+                    typeDecimal = True
+                    typeDateTime = True
+                    typeDate = True
+                    typeTime = True
+                    typeBoolean = True
+                    attributeLength = 0
+                    for sourceRow in sourceRows:
+                        attributeValue = sourceRow[colName]
+                        attributeLength = max(attributeLength, len(attributeValue))
+                        try:
+                            int(attributeValue)
+                        except ValueError:
+                            typeInteger = False
+                        try:
+                            float(attributeValue)
+                        except ValueError:
+                            typeDecimal = False
+                        try:
+                            datetimeval = dateparser.parse(attributeValue, default=datetime.min)
+                            if datetimeval.hour or datetimeval.minute:
+                                typeDate = False
+                            # Assume date being min means taken from default, ie not specified in datetime
+                            if datetimeval.date() != datetime.min.date():
+                                typeTime = False
+                        except ValueError:
+                            typeDateTime = False
+                            typeDate = False
+                            typeTime = False
+                        if not attributeValue.lower() in {'true', 'false'}:
+                            typeBoolean = False
+                    if typeInteger:
+                        attributeType = 'integer'
+                    elif typeDecimal:
+                        attributeType = 'decimal'
+                    elif typeBoolean:
+                        attributeType = 'boolean'
+                    elif typeDate:
+                        attributeType = 'date'
+                    elif typeTime:
+                        attributeType = 'time'
+                    elif typeDateTime:
+                        attributeType = 'datetime'
+                    else:
+                        attributeType = 'text'
 
-                norm.con.execute(norm.SourceAttribute.insert(), {
-                    'Id':           attributeId,
-                    'Name':         attributeName,
-                    'Description':  "Created by NVivotools http://barraqda.org/nvivotools/",
-                    'Type':         attributeType,
-                    'Length':       attributeLength,
-                    'CreatedBy':    userId,
-                    'CreatedDate':  datetimeNow,
-                    'ModifiedBy':   userId,
-                    'ModifiedDate': datetimeNow
-                })
-                sourceAttributes[attributeName] = {
-                    'Id':           attributeId,
-                    'Type':         attributeType,
-                    'Length':       attributeLength,
-                }
+                    norm.con.execute(norm.SourceAttribute.insert(), {
+                        'Id':           attributeId,
+                        'Name':         colName,
+                        'Description':  "Created by NVivotools http://barraqda.org/nvivotools/",
+                        'Type':         attributeType,
+                        'Length':       attributeLength,
+                        'CreatedBy':    userId,
+                        'CreatedDate':  datetimeNow,
+                        'ModifiedBy':   userId,
+                        'ModifiedDate': datetimeNow
+                    })
+                    sourceAttributes[colName] = {
+                        'Id':           attributeId,
+                        'Type':         attributeType,
+                        'Length':       attributeLength,
+                    }
+
+            # Does column define a node?
+            elif args.textcolumns and colName in args.textcolumns:
+
+                node = norm.con.execute(select([
+                        norm.Node.c.Id
+                    ]).where(
+                        norm.Node.c.Name == bindparam('Name')
+                    ), {
+                        'Name': colName
+                    }).first()
+                if node:
+                    nodeId = node['Id']
+                else:
+                    nodeId = uuid.uuid4()
+                    norm.con.execute(norm.Node.insert(), {
+                        'Id':           nodeId,
+                        'Name':         colName,
+                        'Description':  "Created by NVivotools http://barraqda.org/nvivotools/",
+                        'CreatedBy':    userId,
+                        'CreatedDate':  datetimeNow,
+                        'ModifiedBy':   userId,
+                        'ModifiedDate': datetimeNow
+                    })
+
+                sourceNodeId[colName] = nodeId
 
         rowNum = 0
         sourcesToInsert      = []
@@ -339,18 +399,38 @@ def editSources(arglist):
                 raw = file(sourcerow['Source'], 'rb').read(32) # at most 32 bytes are returned
                 encoding = chardet.detect(raw)['encoding']
 
-                normSourceRow['Object'] = codecs.open(sourcerow['Source'], 'r', encoding=encoding).read().encode('utf-8')
-                normSourceRow['Content'] = normSourceRow['Object']
+                normSourceRow['Content'] = codecs.open(sourcerow['Source'], 'r', encoding=encoding).read().encode('utf-8')
             else:
                 normSourceRow['ObjectType'] = 'TXT'
+                normSourceRow['Content'] = (sourceRow.get('Text') or '').encode('utf-8')
 
-                normSourceRow['Object']  = buffer(sourceRow.get('Text', '').encode('utf-8'))
-                normSourceRow['Content'] = normSourceRow['Object']
-
+            normSourceRow['Object']  = buffer(normSourceRow['Content'])
 
             # Skip source without an object
             if not normSourceRow.get('ObjectType'):
                 continue
+
+            for textColumn in args.textcolumns:
+                normSourceText = sourceRow.get(textColumn) or ''
+                if normSourceText:
+                    nodeId = sourceNodeId[textColumn]
+                    start  = len(normSourceRow['Content']) + 1
+                    end    = start + len(normSourceText) - 1
+                    normSourceText += '\n'
+                    normSourceRow['Content'] += normSourceText
+
+                    taggingId = uuid.uuid4()
+                    norm.con.execute(norm.Tagging.insert(), {
+                            'Id':           taggingId,
+                            'Source':       sourceId,
+                            'Node':         nodeId,
+                            'Fragment':     str(start) + ':' + str(end),
+                            'Memo':         None,
+                            'CreatedBy':    userId,
+                            'CreatedDate':  datetimeNow,
+                            'ModifiedBy':   userId,
+                            'ModifiedDate': datetimeNow
+                        })
 
             if source is None:    # New source
                 normSourceRow.update({
